a0cd88af5930f094326ee70c70cc4e436b094bcc
大菠萝 <2062152083@qq.com>
2018-05-09 14:25:46 +0800

    Create fntTool.py

---
+++ fnt/fntTool.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+# 到了 生成美术字的图片集fnt
+import os
+#import PIL
+from PIL import Image
+import sys, os, glob, math,codecs
+
+colCount = 1 #列数
+rowCount = 1 #行数
+max_height=0
+max_width=0
+
+#默认文本fnt字体
+face = "Arial"
+
+def create_fnt_file(fnt_name, fnt_define):
+    global face
+    write_file=open(fnt_name,"w")
+    write_file = codecs.open(fnt_name,"w","utf-8")
+    #face="Arial”,字体为”Arial”
+    # size=32:大小为32像素
+    # bold=0 :不加粗
+    # italic=0:不使用斜体
+    # charset="": charset是编码字符集，这里没有填写值即使用默认，
+    # unicode=0:不使用Unicode
+    # stretchH=100:纵向缩放百分比
+    # smooth=1 :开启平滑
+    # aa=1:开启抗锯齿
+    # padding=0,0,0,0:内边距，文字与边框的空隙。
+    # spacing=1,1 :外边距，就是相临边缘的距离。
+    head_msg1="""info face="%s" size=%s bold=0 italic=0 charset="" unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=2,2 outline=0\n""" % (face,fnt_define["size"])
+    write_file.write(head_msg1)
+    # lineHeight=37：行高，如果遇到换行符时，绘制字的位置坐标的Y值在换行后增加的像素值。
+    # base=28 :字的基本大小
+    # scaleW=512 :图片大小
+    # scaleH=512:图片大小
+    # pages=1 :此种字体共用到几张图。
+    # packed=0:图片不压缩
+    head_msg2 ="""common lineHeight=%s base=%s scaleW=%s scaleH=%s pages=1 packed=0 alphaChnl=0 redChnl=0 greenChnl=0 blueChnl=0\n""" % (fnt_define["lineHeight"],fnt_define["base"],fnt_define["scaleW"],fnt_define["scaleH"])
+    write_file.write(head_msg2)
+    # //第一页，文件名称是”bitmapFontChinese.png”
+    # page id=0 file="bitmapFontChinese.png"
+    head_msg3 = """page id=0 file="%s"\n""" % (fnt_define["file"])
+    write_file.write(head_msg3)
+    # 第四行是当前贴图中所容纳的文字数量
+    head_msg4 = """chars count=%s\n""" % (fnt_define["count"])
+    write_file.write(head_msg4)
+
+    for i in range(0,int(fnt_define["count"])):
+        data=fnt_define["data"][i]
+        line="char id=%s x=%s y=%s width=%s height=%s xoffset=%s yoffset=%s xadvance=%s page=%s chnl=%s letter=\"%s\"\n" %(data["id"],data["x"],data["y"],data["width"],data["height"],data["xoffset"],data["yoffset"],data["xadvance"],data["page"],data["chnl"],data["letter"])
+        write_file.write(line)
+
+
+def joint_image(image_name,convert_list):
+    # 引用全局变量
+    outW=max_width*colCount
+    outH=max_height*rowCount
+
+    print("out image size %dx%d" %(outW,outH))
+
+    toImage = Image.new('RGBA', (outW, outH))
+
+    x=0
+    index = 0
+    for key in convert_list.keys():
+        fromImage=Image.open(key)
+        offsetX = (index%colCount)*max_width
+        offsetY = int(int(index/colCount)*max_height)
+        toImage.paste(fromImage,( offsetX, offsetY))
+        # print("\t %s offset %d %d" %(key,offsetX, offsetY))
+        x+=fromImage.size[0]
+        index += 1
+
+    toImage.save(image_name)
+
+
+def make_fnt_file(pre_str,convert_list,output_path_name):
+    # 引用全局变量
+    global max_width
+    global max_height
+    global colCount
+    global rowCount
+    fnt_name=output_path_name+"/"+pre_str+".fnt"
+    image_name=pre_str+".png"
+    fnt_define=dict()
+    index=0
+    xOffset=0
+    max_height=0
+    max_width=0
+    for key in convert_list.keys():
+        image = Image.open(key)
+        print("image %s   size: w{%d} h{%d}"%(key,image.size[0],image.size[1]))
+        max_width=max(max_width,image.size[0])
+        max_height=max(max_height,image.size[1])
+    totalCount = len(convert_list)
+    colCount =  int(math.ceil(math.sqrt(max_height*max_width*totalCount*1.0)/max_width))
+    rowCount = int(math.ceil(len(convert_list)*1.0/colCount))
+    print("--------------------colxrow---", colCount, rowCount, totalCount)
+
+    fnt_define_item=list()
+    for key in convert_list.keys():
+        # print("\t+"+key+"\t"+"Key:"+chr(int(convert_list[key])))
+        image = Image.open(key)
+        image_size= image.size
+        fnt_define_item_data=dict()
+        fnt_define_item_data["id"]=convert_list[key]
+        fnt_define_item_data["x"]=str((index%colCount)*max_width)
+        fnt_define_item_data["y"]=str(int(index/colCount)*max_height)
+        fnt_define_item_data["width"]=str(image_size[0])
+        fnt_define_item_data["height"]=str(image_size[1])
+        fnt_define_item_data["xoffset"]=str(0)
+        fnt_define_item_data["yoffset"]=str(0)
+        fnt_define_item_data["xadvance"]=str(image_size[0]) #???
+        fnt_define_item_data["page"]=str(0)
+        fnt_define_item_data["chnl"]=str(0)
+        fnt_define_item_data["letter"]=chr(int(convert_list[key]))
+
+        fnt_define_item.append(fnt_define_item_data)
+
+        index+=1
+        xOffset+=image_size[0]
+        max_width=max(max_width,image_size[0])
+        max_height=max(max_height,image_size[1])
+
+    fnt_define["data"]=fnt_define_item
+    fnt_define["size"]=str(max_width)
+    fnt_define["lineHeight"]=str(max_height)
+    fnt_define["base"]=str(max_width)
+    fnt_define["scaleW"]=str(max_width*colCount)
+    fnt_define["scaleH"]=str(max_height*rowCount)
+    fnt_define["file"]=image_name
+    fnt_define["count"]=len(convert_list)
+
+    image_name=output_path_name+"/"+image_name
+
+    create_fnt_file(fnt_name, fnt_define)
+    print ("make:"+fnt_name+"done!")
+    joint_image(image_name,convert_list)
+    print("make {%s} fnt done! good!"%(image_name))
+
+
+def check_and_make(fnt_name,convert_list,output_path_name):
+    if len(convert_list)>=1 :
+        print("*************************************************************")
+        print("fnt file: " +fnt_name+" ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
+        make_fnt_file(fnt_name,convert_list,output_path_name)
+
+
+def main():
+    # 遍历当前文件夹，直接使用目录作为fnt的名字
+    #输出目录
+    output_path_name="output"
+    resourePath = "./"
+    for parent,dirnames,filenames in os.walk(resourePath):
+        # print(parent)
+        # print(dirnames)
+        # print(filenames)
+        # 取第一层目录即可
+        for dir_list in dirnames:
+            srcpath = resourePath + dir_list
+            print("start dir name:"+srcpath)
+            output_path_name = srcpath+os.sep+"output"
+            if not os.path.exists(output_path_name):
+                os.makedirs(output_path_name)
+
+            convert_list=dict()
+            fnt_name = os.path.basename(srcpath)
+            # 存对应的ascii码
+            for filename in glob.glob(srcpath+os.sep+"*.png"):
+                basename = os.path.basename(filename)
+                font=basename[:basename.rfind(".")]
+                # utf-8 下需要调用UTF-8编码
+                # ascii_code = ascii_code.decode('UTF-8')
+                try:
+                    ascii_code = ord(font)
+                except TypeError as e:
+                    print("font define name:" + font + " error !!")
+                    return
+                print("font " + font +  " ascii_code : %d" %(ascii_code))
+                # print ascii_code
+                convert_list[filename]=ascii_code
+                continue
+            check_and_make(fnt_name,convert_list,output_path_name)
+        # 取第一层目录即可
+        break
+
+
+if __name__ == '__main__':
+    main()
